fdcfa1f1635cd7af20bb925e29d71a6fd0eb6022
Palak-Sonkar-20 <palaksonkar20@gmail.com>
2026-03-24 16:12:50 +0530

    AQ1

--- array/findmaxmin.cpp
+++ array/findmaxmin.cpp
@@ -6,11 +6,21 @@ void findmax(int arr[],int n){
         if(max<arr[i])
             max=arr[i];
     }
-    cout<<"max element is: "<< max;
+    cout<<"max element is: "<< max<<endl;
+}
+void findmin(int arr[], int n){
+    int min = arr[0];
+    for(int i =1 ; i<n ; i++){
+        if(min > arr[i]){
+            min = arr[i];
+        }
+    }
+    cout<< "min element is :"<< min<<endl;
 }
 int main(){
     int pal[]={1,23,60,29,5};
     int n = sizeof(pal)/sizeof(pal[0]);
     findmax(pal,n);
+    findmin(pal,n);
     return 0;
 }
